ca0ea955f314bf53e74f483a35821ae68042aca9
Dewey Dunnington <dewey@fishandwhistle.net>
2021-12-04 12:50:36 -0400

    export my_function

--- R/my-function.R
+++ R/my-function.R
@@ -1,4 +1,14 @@
 
+#' A Wrapper for a 'C' Function
+#'
+#' @param input An input value to the C function
+#'
+#' @return The result of the C function.
+#' @export
+#'
+#' @examples
+#' my_function("some input")
+#'
 my_function <- function(input) {
   .Call(anrpackageusingc_c_my_function, input)
 }
